16f30f352a84bf4625d62d7589798c895daf06cf
1C0DER <106778213+1C0DER@users.noreply.github.com>
2024-11-24 16:37:54 +0000

    Add project files.

---
+++ Compiler/Tokenization/TokenizerTestInProgress.cs
@@ -0,0 +1,33 @@
+﻿using Compiler.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Tokenization
+{
+    class TokenizerTest
+    {
+        static void Main(string[] args)
+        {
+            List<string> testInputs = new List<string>
+        {
+            "$abc$", "abc123", "123abc", "1,234", "1,,234",
+            "'a'", "'ab'", "+", ":", ":=", "!", "if x > 0"
+        };
+
+            Tokenizer tokenizer = new Tokenizer(new StringReader(string.Join(" ", testInputs)), new ErrorReporter());
+
+            Console.WriteLine("Testing Tokenizer:");
+            List<Token> tokens = tokenizer.GetAllTokens();
+
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token.ToString());
+            }
+        }
+    }
+
+}
